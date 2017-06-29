151dae1aaecc7938692559453427f8b2eac13a20
neil <neil.figg@gmail.com>
2017-06-30 07:45:55 +1000

    Removed duplicate fn call from projectile config

--- init.el
+++ init.el
@@ -131,8 +131,7 @@
   (setq projectile-cache-file (concat user-emacs-directory "projectile/cache")
         projectile-known-projects-file (concat user-emacs-directory "projectile/bookmarks.eld")
         projectile-use-git-grep t
-        projectile-switch-project-action 'projectile-dired)
-  (projectile-global-mode))
+        projectile-switch-project-action 'projectile-dired))
 
 (use-package s
  :ensure t)
@@ -397,3 +396,17 @@
 (set-terminal-coding-system 'utf-8)
 
 (prefer-coding-system 'utf-8)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (clojure-snippets clj-refactor cider clojure-mode yasnippet which-key use-package spinner seq s rainbow-mode rainbow-delimiters queue paredit-everywhere no-littering neotree multiple-cursors markdown-mode magit ivy-hydra inflections htmlize highlight-parentheses expand-region edn counsel-projectile company bm aggressive-indent ace-window ace-jump-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
