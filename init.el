f88e5966b471eaff2cd091b505e141430ebf8570
Neil Figg <neil.figg@gmail.com>
2017-07-16 16:04:18 +1000

    Added section for Luminus and Leinengen

--- init.el
+++ init.el
@@ -94,7 +94,8 @@
     (cljr-add-keybindings-with-prefix "C-c C-m")))
 
 (use-package clojure-snippets
-    :ensure t)
+    :ensure t
+)
 
 (use-package company
 :ensure t
@@ -410,17 +411,3 @@
 (set-terminal-coding-system 'utf-8)
 
 (prefer-coding-system 'utf-8)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (htmlize markdown-mode rainbow-mode magit expand-region ace-jump-mode ace-window which-key ivy-hydra counsel-projectile counsel bm dash rainbow-delimiters highlight-parentheses paredit-everywhere aggressive-indent neotree projectile company clojure-snippets use-package no-littering clj-refactor))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
